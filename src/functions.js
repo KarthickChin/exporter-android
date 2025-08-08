57adc75a1e9939625d34858a15dc7c2fc2344617
KarthickChin <karthick@mindvalley.com>
2025-08-08 11:04:54 +0530

    removed mv

--- src/functions.js
+++ src/functions.js
@@ -44,13 +44,13 @@ class ColorObject {
       const name = makeColorName(color);
       let colorName = name;
       if (name.includes('GradientBase')) {
-        colorName = name.replace('GradientBase', colorStyle === ColorStylesEnum.EVE_COLOR_STYLES ? brand + 'GB' : "MV" + 'Gb');
+        colorName = name.replace('GradientBase', colorStyle === ColorStylesEnum.EVE_COLOR_STYLES ? brand + 'GB' : 'Gb');
       } else if (name.includes('Alpha')) {
-        colorName = colorStyle === ColorStylesEnum.EVE_COLOR_STYLES ? brand + color.parent.name + color.name : "mv" + color.parent.name + color.name;
+        colorName = colorStyle === ColorStylesEnum.EVE_COLOR_STYLES ? brand + color.parent.name + color.name : color.parent.name + color.name;
       } else if (colorStyle === ColorStylesEnum.EVE_COLOR_STYLES) {
         colorName = brand + color.name;
       } else if (colorStyle === ColorStylesEnum.COLOR_STYLES) {
-        colorName = ColorObject.isiOSSystemColor(color.name) ? "mv" + color.name : color.name;
+        colorName = ColorObject.isiOSSystemColor(color.name) ? color.name : color.name;
       }
       //console.log("ColorObject: " + colorName);
       return new ColorObject(theme.name, color.value, colorStyle, colorName);
