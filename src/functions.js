bfecea75d03269ac8bdcf7bb15591f7de58dd0dc
KarthickChin <karthick@mindvalley.com>
2025-10-01 13:43:58 +0530

    name changes

--- src/functions.js
+++ src/functions.js
@@ -44,7 +44,7 @@ class ColorObject {
       const name = makeColorName(color);
       let colorName = name;
       if (name.includes('GradientBase')) {
-        colorName = name.replace('GradientBase', colorStyle === ColorStylesEnum.EVE_COLOR_STYLES ? brand + 'GB' : color.parent.name +'Gb');
+        colorName = name.replace('GradientBase', colorStyle === ColorStylesEnum.EVE_COLOR_STYLES ? brand + 'GB' : 'Gb');
       } else if (name.includes('Alpha')) {
         colorName = colorStyle === ColorStylesEnum.EVE_COLOR_STYLES ? brand + color.name : color.parent.name + color.name;
       } else if (colorStyle === ColorStylesEnum.EVE_COLOR_STYLES) {
